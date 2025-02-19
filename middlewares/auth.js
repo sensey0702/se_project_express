70d039990af6b958df39e4e187d8866728e3767b
Sam Ensey <168656118+sensey0702@users.noreply.github.com>
2025-02-18 22:32:15 -0600

    changes to middlewares/auth.js and utils/errors.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,22 @@
 const jwt = require("jsonwebtoken");
-const { UNAUTHORIZED_STATUS_CODE } = require("../utils/errors");
+// const { UNAUTHORIZED_STATUS_CODE } = require("../utils/errors");
 const { JWT_SECRET } = require("../utils/config");
+const UnauthorizedError = require("../errors/UnauthorizedError");
 
 const handleAuthError = (res) => {
-  res
-    .status(UNAUTHORIZED_STATUS_CODE)
-    .send({ message: "Authorization Required" });
+  //   res
+  //     .status(UNAUTHORIZED_STATUS_CODE)
+  //     .send({ message: "Authorization Required" });
+  throw new UnauthorizedError("Authorizaion Required");
 };
 
 const auth = (req, res, next) => {
+  console.log("Auth middleware called");
   const { authorization } = req.headers; // get authorization from the header
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
     // check if the header exists and starts with 'Bearer '
+    console.log("Auth header missing or incorrect");
     return handleAuthError(res);
   }
   const token = authorization.replace("Bearer ", "");
@@ -20,6 +24,7 @@ const auth = (req, res, next) => {
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
+    console.log("Token verified payload:", payload);
   } catch (err) {
     return handleAuthError(res);
   }
